87d4d31b2747d76ebf056f83c45f9279bb1b8839
apaliakoudis <paliakoudis.alex@gmail.com>
2026-03-23 22:26:31 +0200

    Backend structure

---
+++ backend/ThreatWatch/src/main/java/org/threatwatch/threatwatch/ThreatWatchApplication.java
@@ -0,0 +1,13 @@
+package org.threatwatch.threatwatch;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ThreatWatchApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ThreatWatchApplication.class, args);
+    }
+
+}
---
+++ backend/ThreatWatch/src/test/java/org/threatwatch/threatwatch/ThreatWatchApplicationTests.java
@@ -0,0 +1,13 @@
+package org.threatwatch.threatwatch;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ThreatWatchApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
